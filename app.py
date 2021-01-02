1ee0d24ed7c108782ca29ea0c59e7cc860c049ce
abpuccini <b.atcharaporn@gmail.com>
2021-01-02 12:52:55 -0500

    Added Global API data

--- app.py
+++ app.py
@@ -1,12 +1,50 @@
 from flask import Flask, render_template, jsonify
-
+from flask_sqlalchemy import SQLAlchemy
+from config import database_local
+from os import environ
 
 app = Flask(__name__)
+
+# To prevent app to to sort keys in json file
+app.config['JSON_SORT_KEYS'] = False
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get(
+    'DATABASE_URL', database_local)
+
+db = SQLAlchemy(app)
+
+
+class Global(db.Model):
+    __tablename__ = 'mmr_global'
+    name = db.Column(db.String, primary_key=True)
+    mmr = db.Column(db.Integer)
+    category = db.Column(db.String)
+    location = db.Column(db.String)
+    country = db.Column(db.String)
 
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('index.html')
+
+
+@app.route('/api/mmr-global')
+def getGlobaldata():
+    tasks = db.session.query(Global)
+    mmr_global_data = []
+
+    for task in tasks:
+        item = {
+            'name': task.name,
+            'mmr': task.mmr,
+            'category': task.category,
+            'location': task.location,
+            'country': task.country
+        }
+        mmr_global_data.append(item)
+
+    return jsonify(mmr_global_data)
 
 
 if __name__ == '__main__':
