57d8737ccfc6a274d8926e430b48ca87d21b770e
abpuccini <b.atcharaporn@gmail.com>
2021-01-05 14:37:05 -0500

    Added API for HWC

--- app.py
+++ app.py
@@ -17,6 +17,15 @@ db = SQLAlchemy(app)
 
 
 # Create table schema
+class Global(db.Model):
+    __tablename__ = 'mmr_global'
+    name = db.Column(db.String, primary_key=True)
+    mmr = db.Column(db.Integer)
+    category = db.Column(db.String)
+    latitude = db.Column(db.Integer)
+    longitude = db.Column(db.Integer)
+
+
 class CDC(db.Model):
     __tablename__ = 'mmr_us'
     id = db.Column(db.Integer, primary_key=True)
@@ -27,27 +36,6 @@ class CDC(db.Model):
     births = db.Column(db.Integer)
     maternal_mortality_ratio = db.Column(db.Float)
     population = db.Column(db.Integer)
-
-
-class Global(db.Model):
-    __tablename__ = 'mmr_global'
-    name = db.Column(db.String, primary_key=True)
-    mmr = db.Column(db.Integer)
-    category = db.Column(db.String)
-    latitude = db.Column(db.Integer)
-    longitude = db.Column(db.Integer)
-
-
-# class CDC(db.Model):
-#     __tablename__ = 'mmr_us'
-#     id = db.Column(db.Integer, primary_key=True)
-#     state = db.Column(db.String(255))
-#     state_code = db.Column(db.Integer)
-#     year = db.Column(db.Integer)
-#     deaths = db.Column(db.Integer)
-#     births = db.Column(db.Integer)
-#     maternal_mortality_ratio = db.Column(db.Float)
-#     population = db.Column(db.Integer)
 
 
 class Ins(db.Model):
@@ -62,6 +50,105 @@ class Ins(db.Model):
     military = db.Column(db.Float)
     uninsured = db.Column(db.Float)
     total = db.Column(db.Integer)
+
+
+class HWC(db.Model):
+    __tablename__ = 'hwc_data'
+    state_name = db.Column(db.String(255), primary_key=True)
+    ahi_rank = db.Column(db.Float)
+    ahi_value = db.Column(db.Float)
+    ahi_score = db.Column(db.Float)
+    ahi_lower_ci = db.Column(db.Float)
+    ahi_upper_ci = db.Column(db.Float)
+    ai_rank = db.Column(db.Float)
+    ai_value = db.Column(db.Float)
+    ai_score = db.Column(db.Float)
+    ai_lower_ci = db.Column(db.Float)
+    ai_upper_ci = db.Column(db.Float)
+    apc_rank = db.Column(db.Float)
+    apc_value = db.Column(db.Float)
+    apc_score = db.Column(db.Float)
+    apc_lower_ci = db.Column(db.Float)
+    apc_upper_ci = db.Column(db.Float)
+    ac_rank = db.Column(db.Float)
+    ac_value = db.Column(db.Float)
+    ac_score = db.Column(db.Float)
+    ac_lower_ci = db.Column(db.Float)
+    ac_upper_ci = db.Column(db.Float)
+    dhcp_rank = db.Column(db.Float)
+    dhcp_value = db.Column(db.Float)
+    dhcp_score = db.Column(db.Float)
+    dhcp_lower_ci = db.Column(db.Float)
+    dhcp_upper_ci = db.Column(db.Float)
+    ds_rank = db.Column(db.Float)
+    ds_value = db.Column(db.Float)
+    ds_score = db.Column(db.Float)
+    ds_lower_ci = db.Column(db.Float)
+    ds_upper_ci = db.Column(db.Float)
+    mpinc_rank = db.Column(db.Float)
+    mpinc_value = db.Column(db.Float)
+    mpinc_score = db.Column(db.Float)
+    mpinc_lower_ci = db.Column(db.Float)
+    mpinc_upper_ci = db.Column(db.Float)
+    mow_rank = db.Column(db.Float)
+    mow_value = db.Column(db.Float)
+    mow_score = db.Column(db.Float)
+    mow_lower_ci = db.Column(db.Float)
+    mow_upper_ci = db.Column(db.Float)
+    pw_rank = db.Column(db.Float)
+    pw_value = db.Column(db.Float)
+    pw_score = db.Column(db.Float)
+    pw_lower_ci = db.Column(db.Float)
+    pw_upper_ci = db.Column(db.Float)
+    ow_rank = db.Column(db.Float)
+    ow_value = db.Column(db.Float)
+    ow_score = db.Column(db.Float)
+    ow_lower_ci = db.Column(db.Float)
+    ow_upper_ci = db.Column(db.Float)
+    ppv_rank = db.Column(db.Float)
+    ppv_value = db.Column(db.Float)
+    ppv_score = db.Column(db.Float)
+    ppv_lower_ci = db.Column(db.Float)
+    ppv_upper_ci = db.Column(db.Float)
+    pctt_rank = db.Column(db.Float)
+    pctt_value = db.Column(db.Float)
+    pctt_score = db.Column(db.Float)
+    pctt_lower_ci = db.Column(db.Float)
+    pctt_upper_ci = db.Column(db.Float)
+    pfhs_rank = db.Column(db.Float)
+    pfhs_value = db.Column(db.Float)
+    pfhs_score = db.Column(db.Float)
+    pfhs_lower_ci = db.Column(db.Float)
+    pfhs_upper_ci = db.Column(db.Float)
+    rpa_rank = db.Column(db.Float)
+    rpa_value = db.Column(db.Float)
+    rpa_score = db.Column(db.Float)
+    rpa_lower_ci = db.Column(db.Float)
+    rpa_upper_ci = db.Column(db.Float)
+    uw_rank = db.Column(db.Float)
+    uw_value = db.Column(db.Float)
+    uw_score = db.Column(db.Float)
+    uw_lower_ci = db.Column(db.Float)
+    uw_upper_ci = db.Column(db.Float)
+    wwv_rank = db.Column(db.Float)
+    wwv_value = db.Column(db.Float)
+    wwv_score = db.Column(db.Float)
+    wwv_lower_ci = db.Column(db.Float)
+    wwv_upper_ci = db.Column(db.Float)
+    wic_rank = db.Column(db.Float)
+    wic_value = db.Column(db.Float)
+    wic_score = db.Column(db.Float)
+    wic_lower_ci = db.Column(db.Float)
+    wic_upper_ci = db.Column(db.Float)
+
+
+class Key(db.Model):
+    __tablename__ = 'hwc_key'
+    measure_name = db.Column(db.String(255), primary_key=True)
+    abbreviation = db.Column(db.String(255))
+    demographic_breakdown = db.Column(db.String(255))
+    source = db.Column(db.String(255))
+    source_year = db.Column(db.String(255))
 
 
 @app.route('/')
@@ -146,6 +233,157 @@ def getInsdata():
         ins_us_data.append(item)
 
     return jsonify(ins_us_data)
+
+
+@app.route('/api/hwc-key')
+def getHWCkey():
+    tasks = db.session.query(Key)
+    key_hwc_data = []
+
+    for task in tasks:
+        item = {
+            'measure_name': task.measure_name,
+            'abbreviation': task.abbreviation,
+            'demographic_breakdown': task.demographic_breakdown,
+            'source': task.source,
+            'source_year': task.source_year
+        }
+        key_hwc_data.append(item)
+
+    return jsonify(key_hwc_data)
+
+
+@app.route('/api/hwc')
+def getHWCdata():
+    tasks = db.session.query(HWC)
+    hwc_data = []
+
+    for task in tasks:
+        item = {
+            'state': task.state_name,
+            'ahi': {
+                'rank': task.ahi_rank,
+                'value': task.ahi_value,
+                'score': task.ahi_score,
+                'lower_ci': task.ahi_lower_ci,
+                'upper_ci': task.ahi_upper_ci
+            },
+            'ai': {
+                'rank': task.ai_rank,
+                'value': task.ai_value,
+                'score': task.ai_score,
+                'lower_ci': task.ai_lower_ci,
+                'upper_ci': task.ai_upper_ci
+            },
+            'apc': {
+                'rank': task.apc_rank,
+                'value': task.apc_value,
+                'score': task.apc_score,
+                'lower_ci': task.apc_lower_ci,
+                'upper_ci': task.apc_upper_ci
+            },
+            'ac': {
+                'rank': task.ac_rank,
+                'value': task.ac_value,
+                'score': task.ac_score,
+                'lower_ci': task.ac_lower_ci,
+                'upper_ci': task.ac_upper_ci
+            },
+            'dhcp': {
+                'rank': task.dhcp_rank,
+                'value': task.dhcp_value,
+                'score': task.dhcp_score,
+                'lower_ci': task.dhcp_lower_ci,
+                'upper_ci': task.dhcp_upper_ci
+            },
+            'ds': {
+                'rank': task.ds_rank,
+                'value': task.ds_value,
+                'score': task.ds_score,
+                'lower_ci': task.ds_lower_ci,
+                'upper_ci': task.ds_upper_ci
+            },
+            'mpinc': {
+                'rank': task.mpinc_rank,
+                'value': task.mpinc_value,
+                'score': task.mpinc_score,
+                'lower_ci': task.mpinc_lower_ci,
+                'upper_ci': task.mpinc_upper_ci
+            },
+            'mow': {
+                'rank': task.mow_rank,
+                'value': task.mow_value,
+                'score': task.mow_score,
+                'lower_ci': task.mow_lower_ci,
+                'upper_ci': task.mow_upper_ci
+            },
+            'pw': {
+                'rank': task.pw_rank,
+                'value': task.pw_value,
+                'score': task.pw_score,
+                'lower_ci': task.pw_lower_ci,
+                'upper_ci': task.pw_upper_ci
+            },
+            'ow': {
+                'rank': task.ow_rank,
+                'value': task.ow_value,
+                'score': task.ow_score,
+                'lower_ci': task.ow_lower_ci,
+                'upper_ci': task.ow_upper_ci
+            },
+            'ppv': {
+                'rank': task.ppv_rank,
+                'value': task.ppv_value,
+                'score': task.ppv_score,
+                'lower_ci': task.ppv_lower_ci,
+                'upper_ci': task.ppv_upper_ci
+            },
+            'pctt': {
+                'rank': task.pctt_rank,
+                'value': task.pctt_value,
+                'score': task.pctt_score,
+                'lower_ci': task.pctt_lower_ci,
+                'upper_ci': task.pctt_upper_ci
+            },
+            'pfhs': {
+                'rank': task.pfhs_rank,
+                'value': task.pfhs_value,
+                'score': task.pfhs_score,
+                'lower_ci': task.pfhs_lower_ci,
+                'upper_ci': task.pfhs_upper_ci
+            },
+            'rpa': {
+                'rank': task.rpa_rank,
+                'value': task.rpa_value,
+                'score': task.rpa_score,
+                'lower_ci': task.rpa_lower_ci,
+                'upper_ci': task.rpa_upper_ci
+            },
+            'uw': {
+                'rank': task.uw_rank,
+                'value': task.uw_value,
+                'score': task.uw_score,
+                'lower_ci': task.uw_lower_ci,
+                'upper_ci': task.uw_upper_ci
+            },
+            'wwv': {
+                'rank': task.wwv_rank,
+                'value': task.wwv_value,
+                'score': task.wwv_score,
+                'lower_ci': task.wwv_lower_ci,
+                'upper_ci': task.wwv_upper_ci
+            },
+            'wic': {
+                'rank': task.wic_rank,
+                'value': task.wic_value,
+                'score': task.wic_score,
+                'lower_ci': task.wic_lower_ci,
+                'upper_ci': task.wic_upper_ci
+            }
+        }
+        hwc_data.append(item)
+
+    return jsonify(hwc_data)
 
 
 if __name__ == '__main__':
