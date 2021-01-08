7d14b3334a6d227c22ef66b5a5d3f11700c1915c
abpuccini <b.atcharaporn@gmail.com>
2021-01-08 15:02:12 -0500

    Added Note

--- static/js/us.js
+++ static/js/us.js
@@ -1,31 +1,31 @@
 // Initialize arrays to hold data
 var states = [];
 var usData = [];
-var insData = []; 
+var insData = [];
 
 // Function to call data when the webpage loads
 function init() {
     // Append options for users to select based on ids
     d3.json("/api/ins-us").then(function (ins) {
-        d3.json("/api/mmr-us").then(function(data){
+        d3.json("/api/mmr-us").then(function (data) {
             usData = data;
             insData = ins
             console.log(insData);
             var filterState = ins.filter(event => event.year === 2009);
-        filterState.forEach(element => {
-            state = element.location;
-            states.push(state);
-        });
-        var selection = d3.select("#selDataset");
+            filterState.forEach(element => {
+                state = element.location;
+                states.push(state);
+            });
+            var selection = d3.select("#selDataset");
 
-        states.forEach(item => {
-            var options = selection.append("option");
-            options.property("value", item);
-            options.text(item);
+            states.forEach(item => {
+                var options = selection.append("option");
+                options.property("value", item);
+                options.text(item);
+            });
+            buildPlot(selection.property("value"));
+            insChart(selection.property("value"));
         });
-        buildPlot(selection.property("value"));
-        insChart(selection.property("value"));
-        });            
     });
 };
 
@@ -39,63 +39,63 @@ function optionChanged(state) {
 };
 
 
- 
+
 
 function buildPlot(state) {
-        var filterData = usData.filter(event => event.state === state);
-        var mmrData = [];
-        var yearData = [];
-        var birthData = [];
-        var deathData = [];
-        filterData.forEach(item => {
-            mmr = item.mmr;
-            year = item.year;
-            birth = item.births;
-            death = item.deaths;
-            mmrData.push(mmr);
-            yearData.push(year);
-            birthData.push(birth);
-            deathData.push(death);
-        });
+    var filterData = usData.filter(event => event.state === state);
+    var mmrData = [];
+    var yearData = [];
+    var birthData = [];
+    var deathData = [];
+    filterData.forEach(item => {
+        mmr = item.mmr;
+        year = item.year;
+        birth = item.births;
+        death = item.deaths;
+        mmrData.push(mmr);
+        yearData.push(year);
+        birthData.push(birth);
+        deathData.push(death);
+    });
 
-        var trace = {
-            x: yearData, 
-            y: mmrData,
-            name: "MMR",
-            type: "scatter",
-            //range: [0,70],
-            //text: birthData,
-            //text: `<b>Births: ${birthData}</b>`
-        };
-
-        var data = [trace];
-
-        var layout = {
-            title: `<b>${state} Maternal Mortality Ratio</b>`,
-            yaxis: {
-                title: "<b>MMR</b>"
-            },
-            xaxis: {
-                title: "<b>Year</b><br>*In 2014 ACA Insurance Reforms and ACA Marketplace <br>premium subsidies took effect. Medicaid expansion began in states that opted in.",
-                type: "date"
-            },
-            shapes: [
-                {
-                    type: "line", 
-                    x0: "2014",
-                    y0:0,
-                    x1:"2014",
-                    y1:70,
-                    line:{
-                        dash: "dot"
-                    } 
-        
-                },
-                ]               
-        };
-
-        Plotly.newPlot("plot", data, layout, {displayModeBar: false});
+    var trace = {
+        x: yearData,
+        y: mmrData,
+        name: "MMR",
+        type: "scatter",
+        //range: [0,70],
+        //text: birthData,
+        //text: `<b>Births: ${birthData}</b>`
     };
+
+    var data = [trace];
+
+    var layout = {
+        title: `<b>${state} Maternal Mortality Ratio</b>`,
+        yaxis: {
+            title: "<b>MMR</b>"
+        },
+        xaxis: {
+            title: "<b>Year</b>",
+            type: "date"
+        },
+        shapes: [
+            {
+                type: "line",
+                x0: "2014",
+                y0: 0,
+                x1: "2014",
+                y1: 70,
+                line: {
+                    dash: "dot"
+                }
+
+            },
+        ]
+    };
+
+    Plotly.newPlot("plot", data, layout, { displayModeBar: false });
+};
 
 // function buildChart() {
 //     d3.json("/api/ins-us").then(function(data){
@@ -113,9 +113,9 @@ function buildPlot(state) {
 // });  
 
 function insChart(state) {
-    var filterData = insData.filter(choice => 
+    var filterData = insData.filter(choice =>
         choice.location === state);
-        console.log(filterData);
+    console.log(filterData);
     var employerData = [];
     var medicaidData = [];
     var medicareData = [];
@@ -123,13 +123,13 @@ function insChart(state) {
     var nonGroupData = [];
     var uninsuredData = [];
     var yearData = [];
-//filter usData for MMR rate to add to insurance chart
+    //filter usData for MMR rate to add to insurance chart
     var filterMMRData = usData.filter(event => event.state === state);
-        var mmrData = [];
-        filterMMRData.forEach(item => {
-            mmr = item.mmr;
-            mmrData.push(mmr);
-        });    
+    var mmrData = [];
+    filterMMRData.forEach(item => {
+        mmr = item.mmr;
+        mmrData.push(mmr);
+    });
 
     filterData.forEach(item => {
         employer = item.employer;
@@ -137,7 +137,7 @@ function insChart(state) {
         medicare = item.medicare;
         military = item.military;
         nonGroup = item.non_group;
-        uninsured = item.uninsured; 
+        uninsured = item.uninsured;
         year = item.year;
 
         employerData.push(employer);
@@ -146,7 +146,7 @@ function insChart(state) {
         militaryData.push(military);
         nonGroupData.push(nonGroup);
         uninsuredData.push(uninsured);
-        yearData.push(year);    
+        yearData.push(year);
     });
     // missing years for MMR data is problematic for graphing alongside ins Data
     // var trace1 = {
@@ -157,7 +157,7 @@ function insChart(state) {
     // }
 
     var trace2 = {
-        x: yearData, 
+        x: yearData,
         y: employerData,
         name: "Employer Plans",
         // mode: "markers",
@@ -187,7 +187,7 @@ function insChart(state) {
 
     var trace6 = {
         x: yearData,
-        y: uninsuredData, 
+        y: uninsuredData,
         name: "Uninsured",
         type: "scatter",
     }
@@ -203,25 +203,25 @@ function insChart(state) {
             //range: [1,30],
         },
         xaxis: {
-            title: "<b>Year</b><br>*In 2014 ACA Insurance Reforms and ACA Marketplace <br>premium subsidies took effect. Medicaid expansion began in states that opted in.",
+            title: "<b>Year</b>",
             type: "date"
         },
         shapes: [
-        {
-            type: "line", 
-            x0: "2014",
-            y0:0,
-            x1:"2014",
-            y1:25,
-            line:{
-                dash: "dot"
-            } 
+            {
+                type: "line",
+                x0: "2014",
+                y0: 0,
+                x1: "2014",
+                y1: 25,
+                line: {
+                    dash: "dot"
+                }
 
-        },
+            },
         ]
     };
 
-    Plotly.newPlot("insChart", data, layout, {displayModeBar: false});
+    Plotly.newPlot("insChart", data, layout, { displayModeBar: false });
 };
 
 // insChart();
